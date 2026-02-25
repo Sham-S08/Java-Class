5b2e8f1c8c99c2c6d183b02d90cafa3899d5b8a4
Shambhavi Srivastava <shambhavi.cd231094@global.org.in>
2026-02-25 19:02:07 +0530

    Added Day1 Java file

---
+++ Day1.java
@@ -0,0 +1,9 @@
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting DSA with Java 🚀");
+        System.out.println("Java setup is working perfectly!");
+
+    }
+}
