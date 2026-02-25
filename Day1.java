eb3f2fc99ab490dd49fe5d54537cf4f0fe34abf7
Shambhavi Srivastava <shambhavi.cd231094@global.org.in>
2026-02-25 19:58:45 +0530

    Setup Complete!~

--- Day1.java
+++ Day1.java
@@ -2,7 +2,7 @@ public class Day1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Starting DSA with Java 🚀");
+        System.out.println("Starting DSA with Java~~");
         System.out.println("Java setup is working perfectly!");
 
     }
